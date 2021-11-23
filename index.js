615d32d7394eb0a509a12b4c0a0f24e400116487
Kevin Guerrero <guerrk83z@gmail.com>
2021-11-23 16:30:34 -0500

    commit tree 65c24e5eb288327bd7d6dfafbd3c540ade5b1fe6

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (pickup) {
+    return Math.abs(pickup-42)
+}
+
+function distanceFromHqInFeet (pickup) {
+    return Math.abs(pickup-42)*264
+}
+
+function distanceTravelledInFeet (startBlock, endBlock) {
+    return Math.abs(endBlock-startBlock)*264
+}
+
+function calculatesFarePrice (startBlock, endBlock) {
+    const feet = Math.abs(endBlock-startBlock)*264
+    if (feet < 400) {
+        return 0
+    } else if (feet > 400 && feet < 2000) {
+        return (feet-400)*0.02
+    } else if (feet > 2000 && feet < 2500) {
+        return 25
+    } else {
+        return 'cannot travel that far'
+    }
+     
+}
